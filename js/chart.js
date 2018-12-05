446df00e989c091005b68d933ffb03d553b8a197
Ananya Nijhawan <ananyanijhawan@gmail.com>
2018-12-04 22:27:35 -0500

    update chart

--- js/chart.js
+++ js/chart.js
@@ -61,7 +61,10 @@ $(function() {
   };
   var options = {
     scales: {
-      yAxes: [{
+      yAxes: [
+        
+
+        {
         ticks: {
           beginAtZero: true
         }
@@ -143,20 +146,31 @@ $(function() {
     ]
   };
   var areaData = {
-    labels: ["2013", "2014", "2015", "2016", "2017"],
+    labels: [ "9 AM", "Noon", "3 PM", "6 PM","9 PM","Midnight", "3 AM", "6 AM"],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      label: '# of free washers',
+      data: [12, 19, 3, 5, 2, 8,6,6],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
+        'rgba(0, 191, 255,1)'
       ],
       borderColor: [
-        'rgba(255,99,132,1)',
+        'rgba(0,191,255,1)'
+      ],
+      borderWidth: 1,
+      fill: 'origin', // 0: fill to 'origin'
+      fill: '+2', // 1: fill to dataset 3
+      fill: 1, // 2: fill to dataset 1
+      fill: false, // 3: no fill
+      fill: '-2' // 4: fill to dataset 2
+    },
+    {
+      label: '# of free dryers',
+      data: [10, 14, 13, 2, 2, 7,6,4],
+      backgroundColor: [
+        'rgba(145, 163, 176, 1)'
+      ],
+      borderColor: [
+        'rgba(145, 163, 176, 1)',
         'rgba(54, 162, 235, 1)',
         'rgba(255, 206, 86, 1)',
         'rgba(75, 192, 192, 1)',
@@ -226,7 +240,8 @@ $(function() {
           display: false
         }
       }],
-      yAxes: [{
+      yAxes: [
+        {
         gridLines: {
           display: false
         }
